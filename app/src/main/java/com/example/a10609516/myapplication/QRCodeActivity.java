808ec7c8cd8f4acda1f4a704dfc24c0da399e587
MartinLin08 <martin91218@gamil.com>
2018-03-14 13:57:48 +0800

    新增QRCodeActivity Menu功能

--- app/src/main/java/com/example/a10609516/myapplication/QRCodeActivity.java
+++ app/src/main/java/com/example/a10609516/myapplication/QRCodeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +17,92 @@ public class QRCodeActivity extends AppCompatActivity {
 
     private Button QRCode_btn;
     private TextView date_txt,result_txt;
+
+    /**
+     * 創建Menu
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * 進入Menu各個頁面
+     *
+     * @param item
+     * @return
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_item:
+                Intent intent = new Intent(QRCodeActivity.this, MenuActivity.class);
+                startActivity(intent);
+                Toast.makeText(this, "HOME", Toast.LENGTH_SHORT).show();
+                finish();
+                break; //返回首頁
+            case R.id.schedule_item:
+                Intent intent7 = new Intent(QRCodeActivity.this, ScheduleActivity.class);
+                startActivity(intent7);
+                Toast.makeText(this, "行程資訊", Toast.LENGTH_SHORT).show();
+                break; //進入行程資訊頁面
+            case R.id.calendar_item:
+                Intent intent11 = new Intent(QRCodeActivity.this, CalendarActivity.class);
+                startActivity(intent11);
+                Toast.makeText(this, "派工行事曆", Toast.LENGTH_SHORT).show();
+                break; //顯示派工行事曆
+            case R.id.work_item:
+                Intent intent1 = new Intent(QRCodeActivity.this, SearchActivity.class);
+                startActivity(intent1);
+                Toast.makeText(this, "查詢派工資料", Toast.LENGTH_SHORT).show();
+                break; //進入查詢派工資料頁面
+            /*case R.id.signature_item:
+                Intent intent2 = new Intent(CalendarActivity.this, SignatureActivity.class);
+                startActivity(intent2);
+                Toast.makeText(this, "客戶電子簽名", Toast.LENGTH_SHORT).show();
+                break; //進入客戶電子簽名頁面*/
+            case R.id.record_item:
+                Intent intent8 = new Intent(QRCodeActivity.this, RecordActivity.class);
+                startActivity(intent8);
+                Toast.makeText(this, "上傳日報紀錄", Toast.LENGTH_SHORT).show();
+                break; //進入上傳日報紀錄頁面
+            case R.id.picture_item:
+                Intent intent3 = new Intent(QRCodeActivity.this, PictureActivity.class);
+                startActivity(intent3);
+                Toast.makeText(this, "客戶訂單照片上傳", Toast.LENGTH_SHORT).show();
+                break; //進入客戶訂單照片上傳頁面
+            case R.id.customer_item:
+                Intent intent4 = new Intent(QRCodeActivity.this, CustomerActivity.class);
+                startActivity(intent4);
+                Toast.makeText(this, "客戶訂單查詢", Toast.LENGTH_SHORT).show();
+                break; //進入客戶訂單查詢頁面
+            case R.id.upload_item:
+                Intent intent5 = new Intent(QRCodeActivity.this, UploadActivity.class);
+                startActivity(intent5);
+                Toast.makeText(this, "上傳日報", Toast.LENGTH_SHORT).show();
+                break; //進入上傳日報頁面
+            case R.id.correct_item:
+                Intent intent6 = new Intent(QRCodeActivity.this, CorrectActivity.class);
+                startActivity(intent6);
+                Toast.makeText(this, "日報修正", Toast.LENGTH_SHORT).show();
+                break; //進入日報修正頁面
+            case R.id.about_item:
+                Intent intent9 = new Intent(QRCodeActivity.this, VersionActivity.class);
+                startActivity(intent9);
+                Toast.makeText(this, "版本資訊", Toast.LENGTH_SHORT).show();
+                break; //進入版本資訊頁面
+            case R.id.QRCode_item:
+                Intent intent10 = new Intent(QRCodeActivity.this, QRCodeActivity.class);
+                startActivity(intent10);
+                Toast.makeText(this, "QRCode", Toast.LENGTH_SHORT).show();
+                break; //進入QRCode頁面
+            default:
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
