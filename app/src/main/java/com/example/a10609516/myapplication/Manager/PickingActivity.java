3df83fc72ddf6623fb86c883d300df083f4933f0
MartinLin08 <martin91218@gamil.com>
2019-08-13 16:35:11 +0800

    WQP_Service.APP ver. 1.9 1.增加Tool/WQPServiceActivity 2.Menu、日期挑選器、確認是否有最新版本移入WQPServiceActivity 3.若派工類別為臨時取消，隱藏OK按鈕 4.新增管理部-盤點單InventoryActivity 5.新增管理部-撿料單PickingActivity

---
+++ app/src/main/java/com/example/a10609516/myapplication/Manager/PickingActivity.java
@@ -0,0 +1,15 @@
+package com.example.a10609516.myapplication.Manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a10609516.myapplication.R;
+
+public class PickingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picking);
+    }
+}
